cb26deec1d77defd1bd8d555624254d8091f5a62
BreninhoOMaisMais <breninff10@gmail.com>
2024-09-16 23:32:26 -0300

    TCC

--- vencemed/src/App.js
+++ vencemed/src/App.js
@@ -1,18 +1,36 @@
 import './App.css';
-import Header from './componentes/Header.js';
-import Section from './componentes/Section.js';
-import Footer from './componentes/Footer.js';
+import Logo from './img/logo.png'
 
 function App() {
   return (
     <div className="App">
       <body>
 
-    <Header/>
+      <header>
+        <img src={Logo} alt="Logo" title="Logo do Projeto" />
+        <h1>Conscientização sobre o Descarte de Medicamentos</h1>
+        <nav>
+            <ul>
+                <li><a href="impacto.html">Impacto Ambiental</a></li>
+                <li><a href="perigos.html">Perigos do Descarte Inadequado</a></li>
+                <li><a href="solucoes.html">Soluções</a></li>
+            </ul>
+        </nav>
+        </header>
 
-    <Section/>
+        <section id="perigos">
+        <h2>Perigos do Descarte Inadequado</h2>
+        <p>O descarte de embalagens e remédios em lixo comum ou esgoto pode causar sérios riscos à saúde pública e ao meio ambiente, contribuindo para a contaminação de rios, oceanos e alimentos.</p>
+        <ul>
+            <li>Contaminação de cursos d'água e do solo</li>
+            <li>Riscos à saúde humana e animal</li>
+            <li>Proliferação de doenças e resistência a antibióticos</li>
+        </ul>
+    </section>
 
-    <Footer />
+    <footer>
+            <p>&copy; 2024 - VenceMED</p>
+    </footer>
 
 </body>
     </div>
--- vencemed/src/componentes/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer (){
-    return(
-        <footer>
-            <p>&copy; 2024 - VenceMED</p>
-        </footer>
-    );
-}
-
-export default Footer
